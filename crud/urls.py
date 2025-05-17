1dd0d784489c3b35da7e474498ae8b2b877b6b8a
obalobaoaj <158153334+obalobaoaj@users.noreply.github.com>
2025-05-17 16:55:24 +0800

    logout

--- crud/urls.py
+++ crud/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('login', views.log_in),
+    path('login', views.log_in, name='login'),
     path('gender/list', views.gender_list),
     path('gender/add', views.add_gender),
     path('gender/edit/<int:genderId>', views.edit_gender),
@@ -10,5 +11,6 @@ urlpatterns = [
     path('user/list', views.user_list),
     path('user/add', views.add_user),
     path('user/edit/<int:userId>', views.edit_user),
-    path('user/delete/<int:userId>', views.delete_user)
+    path('user/delete/<int:userId>', views.delete_user),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
